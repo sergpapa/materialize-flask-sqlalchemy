ca4acecf4641c94ef08c52c0cba6c32e8c0c16c5
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-02-01 15:21:28 +0000

    functionality to add_category and relevant routes

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,6 @@
 # Here we are using our Flask routes to render the templates needed
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -13,5 +13,10 @@ def categories():
     return render_template('categories.html')
 
 @app.route('/add_category', methods=['GET', 'POST'])
-def category():
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name = request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template('add_category.html')
