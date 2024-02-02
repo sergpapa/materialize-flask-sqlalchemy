ff3455476556341230a20fd3fbef2e8761a42317
Sergios Papastergiou <serg.papastergiou@gmail.com>
2024-02-02 14:23:01 +0000

    functionality to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var sidenav = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(sidenav);
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    let dropdown = document.querySelectorAll('select');
+    M.FormSelect.init(dropdown);
   });
+
+       
